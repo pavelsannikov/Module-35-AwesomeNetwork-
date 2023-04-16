15f06745370f9980e5574695c851ad20c831363f
Павел Санников <pavelsan95@mail.ru>
2023-04-16 12:22:39 +0500

    Добавьте файлы проекта.

---
+++ AwesomeNetwork/Data/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Data.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(T item);
+    }
+}
